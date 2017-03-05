5f829122c0fc3552f71da896ff615ceeed625bd0
tuxgasy <contact@tuxgasy.fr>
2017-03-06 00:44:30 +0100

    Always create conf file event if auto install is disabled

--- images/4.0.1/docker-run.sh
+++ images/4.0.1/docker-run.sh
@@ -6,6 +6,27 @@ date.timezone = $PHP_INI_DATE_TIMEZONE
 EOF
 fi
 
+if [ ! -f /var/www/html/conf/conf.php ]; then
+	cat <<EOF > /var/www/html/conf/conf.php
+<?php
+\$dolibarr_main_url_root='${DOLI_URL_ROOT}';
+\$dolibarr_main_document_root='/var/www/html';
+\$dolibarr_main_url_root_alt='/custom';
+\$dolibarr_main_document_root_alt='/var/www/html/custom';
+\$dolibarr_main_data_root='/var/www/documents';
+\$dolibarr_main_db_host='${DOLI_DB_HOST}';
+\$dolibarr_main_db_port='3306';
+\$dolibarr_main_db_name='${DOLI_DB_NAME}';
+\$dolibarr_main_db_prefix='llx_';
+\$dolibarr_main_db_user='${DOLI_DB_USER}';
+\$dolibarr_main_db_pass='${DOLI_DB_PASSWORD}';
+\$dolibarr_main_db_type='mysqli';
+EOF
+
+	chown www-data:www-data /var/www/html/conf/conf.php
+	chmod 400 /var/www/html/conf/conf.php
+fi
+
 if [ $DOLI_INSTALL_AUTO -eq 1 ]; then
   r=1
   while [ $r -ne 0 ]; do
@@ -57,26 +78,8 @@ if [ $DOLI_INSTALL_AUTO -eq 1 ]; then
     mysql -u $DOLI_DB_USER -p${DOLI_DB_PASSWORD} -h $DOLI_DB_HOST $DOLI_DB_NAME -e "INSERT INTO llx_const(name,value,type,visible,note,entity) values('MAIN_VERSION_LAST_INSTALL', '${DOLI_VERSION}', 'chaine', 0, 'Dolibarr version when install', 0);" > /dev/null 2>&1
     mysql -u $DOLI_DB_USER -p${DOLI_DB_PASSWORD} -h $DOLI_DB_HOST $DOLI_DB_NAME -e "INSERT INTO llx_const(name,value,type,visible,note,entity) VALUES ('MAIN_LANG_DEFAULT', 'auto', 'chaine', 0, 'Default language', 1);" > /dev/null 2>&1
 
-    echo "Create conf file ..."
-    cat <<EOF > /var/www/html/conf/conf.php
-<?php
-\$dolibarr_main_url_root='${DOLI_URL_ROOT}';
-\$dolibarr_main_document_root='/var/www/html';
-\$dolibarr_main_url_root_alt='/custom';
-\$dolibarr_main_document_root_alt='/var/www/html/custom';
-\$dolibarr_main_data_root='/var/www/documents';
-\$dolibarr_main_db_host='${DOLI_DB_HOST}';
-\$dolibarr_main_db_port='3306';
-\$dolibarr_main_db_name='${DOLI_DB_NAME}';
-\$dolibarr_main_db_prefix='llx_';
-\$dolibarr_main_db_user='${DOLI_DB_USER}';
-\$dolibarr_main_db_pass='${DOLI_DB_PASSWORD}';
-\$dolibarr_main_db_type='mysqli';
-EOF
-
     touch /var/www/documents/install.lock
-    chown -R www-data:www-data /var/www
-    chmod 400 /var/www/html/conf/conf.php
+    chown www-data:www-data /var/www/documents/install.lock
     chmod 400 /var/www/documents/install.lock
   fi
 fi
