e19b28c074987fb2c05a021ca8b2e07e23265b91
Garcia M <garcia@soamichel.fr>
2017-03-06 19:28:45 +0100

    Change user/group ID on run container (not on build)

--- images/4.0.3/docker-run.sh
+++ images/4.0.3/docker-run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+usermod -u $WWW_USER_ID www-data
+groupmod -g $WWW_GROUP_ID www-data
+chown -R www-data:www-data /var/www
+
 if [ ! -f /usr/local/etc/php/php.ini ]; then
   cat <<EOF > /usr/local/etc/php/php.ini
 date.timezone = $PHP_INI_DATE_TIMEZONE
