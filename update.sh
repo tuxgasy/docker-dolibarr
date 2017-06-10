8df6065cdb43bd1cb7707fe0ac721676e303edea
TuxGasy <garcia@soamichel.fr>
2017-06-10 23:49:19 +0200

    add php 7 support

--- update.sh
+++ update.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-versions=( "4.0.0" "4.0.1" "4.0.2" "4.0.3" "4.0.4" "4.0.5" "5.0.0" "5.0.1" )
+versions=( "4.0.0" "4.0.1" "4.0.2" "4.0.3" "4.0.4" "4.0.5" "5.0.0" "5.0.1" "5.0.2" "5.0.3" )
 
 for version in ${versions[@]}; do
   echo "Generate Dockerfile for Dolibarr $version"
 
-  mkdir -p images/$version
+  for php_version in "5.6" "7.0" "7.1"; do
+    if [ "$php_version" = "5.6" ]; then
+      dir="images/${version}"
+    else
+      dir="images/${version}-php${php_version}"
+    fi
 
-  sed 's/%VERSION%/'"$version"'/g;' Dockerfile.template > images/$version/Dockerfile
+    mkdir -p $dir
 
-  cp docker-run.sh images/$version/
+    sed '
+          s/%PHP_VERSION%/'"$php_version"'-apache/;
+          s/%VERSION%/'"$version"'/;
+    ' Dockerfile.template > $dir/Dockerfile
 
-  #docker build -t tuxgasy/dolibarr:$version images/$version/
+    cp docker-run.sh $dir/
+
+    #docker build -t tuxgasy/dolibarr:$version $dir
+  done
 done
