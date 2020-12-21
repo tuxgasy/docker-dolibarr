ba11754df0b5e7114ddb0b164678d2d609dca051
Arnaud Trouche <2650571+Arnaud-Trouche@users.noreply.github.com>
2020-12-21 18:24:26 +0100

    Add LDAP environnement options to conf.php at startup

--- docker-run.sh
+++ docker-run.sh
@@ -35,6 +35,17 @@ EOF
 \$dolibarr_main_db_user='${DOLI_DB_USER}';
 \$dolibarr_main_db_pass='${DOLI_DB_PASSWORD}';
 \$dolibarr_main_db_type='mysqli';
+\$dolibarr_main_authentication='${DOLI_AUTH}';
+\$dolibarr_main_auth_ldap_host='${DOLI_LDAP_HOST}';
+\$dolibarr_main_auth_ldap_port='${DOLI_LDAP_PORT}';
+\$dolibarr_main_auth_ldap_version='${DOLI_LDAP_VERSION}';
+\$dolibarr_main_auth_ldap_servertype='${DOLI_LDAP_SERVER_TYPE}';
+\$dolibarr_main_auth_ldap_login_attribute='${DOLI_LDAP_LOGIN_ATTRIBUTE}';
+\$dolibarr_main_auth_ldap_dn='${DOLI_LDAP_DN}';
+\$dolibarr_main_auth_ldap_filter='${DOLI_LDAP_FILTER}';
+\$dolibarr_main_auth_ldap_admin_login='${DOLI_LDAP_BIND_DN}';
+\$dolibarr_main_auth_ldap_admin_pass='${DOLI_LDAP_BIND_PASS}';
+\$dolibarr_main_auth_ldap_debug='${DOLI_LDAP_DEBUG}';
 EOF
   fi
 
