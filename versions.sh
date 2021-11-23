84db91bc8ebafe8dad397cfa21c4aad895403956
Mathieu POTIER <mathieu.potier@onzeway.eu>
2021-11-23 21:48:28 +0100

    Upgrade to 14.0.4

--- versions.sh
+++ versions.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-DOLIBARR_VERSIONS=( "9.0.4" "10.0.7" "11.0.5" "12.0.5" "13.0.4" "14.0.3" "develop" )
-DOLIBARR_LATEST_TAG="14.0.3"
+DOLIBARR_VERSIONS=( "9.0.4" "10.0.7" "11.0.5" "12.0.5" "13.0.4" "14.0.4" "develop" )
+DOLIBARR_LATEST_TAG="14.0.4"
