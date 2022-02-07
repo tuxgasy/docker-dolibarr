349703510f5f936c64629d0bd7168e2b6dc17b73
David GF <contacto@itsolint.com>
2022-02-07 19:57:08 +0100

    Changing upload_max_filesize and post_max_size using env vars

--- docker-run.sh
+++ docker-run.sh
@@ -42,6 +42,8 @@ function initDolibarr()
 date.timezone = ${PHP_INI_DATE_TIMEZONE}
 sendmail_path = /usr/sbin/sendmail -t -i
 memory_limit = ${PHP_INI_MEMORY_LIMIT}
+upload_max_filesize = ${PHP_INI_UPLOAD_MAX_FILESIZE}
+post_max_size = ${PHP_INI_POST_MAX_SIZE}
 EOF
 
 if [[ ! -f /var/www/html/conf/conf.php ]]; then
