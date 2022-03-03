b7b48ac2b83931f9b5af52f81a7f65f9cd476dcf
Edoardo Zanoni <Ryther@users.noreply.github.com>
2022-03-03 19:01:33 +0100

    Add v15 (#79)

--- versions.sh
+++ versions.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-DOLIBARR_VERSIONS=( "9.0.4" "10.0.7" "11.0.5" "12.0.5" "13.0.4" "14.0.5" "develop" )
-DOLIBARR_LATEST_TAG="14.0.5"
+DOLIBARR_VERSIONS=( "9.0.4" "10.0.7" "11.0.5" "12.0.5" "13.0.4" "14.0.5" "15.0.0" "develop" )
+DOLIBARR_LATEST_TAG="15.0.0"
