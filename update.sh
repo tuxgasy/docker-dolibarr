ee48a7f34398e8a2056ab036391eae6e88b31764
TuxGasy <garcia@soamichel.fr>
2022-06-27 21:53:25 +0200

    Try buildx

--- update.sh
+++ update.sh
@@ -14,6 +14,7 @@ tags=""
 rm -rf "${BASE_DIR}/images" "${BASE_DIR}/docker-compose-links"
 
 if [ "${DOCKER_BUILD}" = "1" ] && [ "${DOCKER_PUSH}" = "1" ]; then
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
   docker buildx create --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --driver docker-container --use
   docker buildx inspect --bootstrap
 fi
