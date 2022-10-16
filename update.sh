2b85b73b60416e036bad3741a51e59fa6e493890
POTIER Mathieu <mathieupotier@users.noreply.github.com>
2022-10-16 23:15:04 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -27,7 +27,7 @@ for dolibarrVersion in "${DOLIBARR_VERSIONS[@]}"; do
 
   # Mapping version according https://wiki.dolibarr.org/index.php/Versions
   # Regarding PHP Supported version : https://www.php.net/supported-versions.php
-  if [[ "${dolibarrMajor}" = "16" || "${dolibarrVersion}" = "develop" ]]; then
+  if [ "${dolibarrMajor}" = "16" ] || [ "${dolibarrVersion}" = "develop" ]; then
     php_base_images=( "8.1-apache-buster" )
   else
     php_base_images=( "7.4-apache-buster" )
