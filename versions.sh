421eb6615e8dd52608f63ec1719c4e765c6527c3
Adrian Bach <65734063+realDayaa@users.noreply.github.com>
2023-02-20 19:05:28 +0100

    upgrade to 16.0.4 (#113)

--- versions.sh
+++ versions.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-DOLIBARR_VERSIONS=( "11.0.5" "12.0.5" "13.0.4" "14.0.5" "15.0.3" "16.0.3" "develop" )
-DOLIBARR_LATEST_TAG="16.0.3"
+DOLIBARR_VERSIONS=( "11.0.5" "12.0.5" "13.0.4" "14.0.5" "15.0.3" "16.0.4" "develop" )
+DOLIBARR_LATEST_TAG="16.0.4"
