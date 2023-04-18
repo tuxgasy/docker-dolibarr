b3bd6c8e77a477837dcc89ae66d2959f1dcb47de
Florent Poinsaut <florent@poinsaut.fr>
2023-04-18 11:51:41 +0200

    Update image v17 to 17.0.1

--- versions.sh
+++ versions.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-DOLIBARR_VERSIONS=( "11.0.5" "12.0.5" "13.0.4" "14.0.5" "15.0.3" "16.0.5" "17.0.0" "develop" )
-DOLIBARR_LATEST_TAG="17.0.0"
+DOLIBARR_VERSIONS=( "11.0.5" "12.0.5" "13.0.4" "14.0.5" "15.0.3" "16.0.5" "17.0.1" "develop" )
+DOLIBARR_LATEST_TAG="17.0.1"
