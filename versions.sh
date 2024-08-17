800874105119ed1cbe275fa162bd11515ed295d2
Benoit Vianin <benoit.vianin@pixeleez.ch>
2024-08-17 13:16:13 +0200

    Add version 19.0.3 of Dolibarr

--- versions.sh
+++ versions.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-DOLIBARR_VERSIONS=("15.0.3" "16.0.5" "17.0.4" "18.0.5" "19.0.2" "develop")
-DOLIBARR_LATEST_TAG="19.0.2"
+DOLIBARR_VERSIONS=("15.0.3" "16.0.5" "17.0.4" "18.0.5" "19.0.3" "develop")
+DOLIBARR_LATEST_TAG="19.0.3"
